eb64959843df25955b3d60f0e848b4caac9d3574
Wandy <wandy.irawan17@gmail.com>
2023-10-08 16:13:45 +0700

    change schema to grovbox

--- lua/wandy/plugins/colorscheme.lua
+++ lua/wandy/plugins/colorscheme.lua
@@ -1,10 +1,12 @@
 return {
-        "rose-pine/neovim",
-        name = 'rose-pine',
-        priority = 1000, 
-        config = function()
-            vim.cmd([[colorscheme rose-pine]])
-            vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
-            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
-        end,
+	-- "rose-pine/neovim",
+	"ellisonleao/gruvbox.nvim",
+	-- name = "rose-pine",
+	priority = 1000,
+	config = function()
+		-- vim.cmd([[colorscheme rose-pine]])
+		vim.cmd([[colorscheme gruvbox]])
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	end,
 }
