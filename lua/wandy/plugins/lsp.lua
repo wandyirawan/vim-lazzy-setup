c0958f3557ec7d3aaf78248ba7f0b8a72cc5a271
Wandy <wandy.irawan17@gmail.com>
2024-01-18 16:50:57 +0700

    fix lsp command not work

--- lua/wandy/plugins/lsp.lua
+++ lua/wandy/plugins/lsp.lua
@@ -24,13 +24,13 @@ return {
 		lsp_zero.on_attach(function(client, bufnr)
 			local opts = { buffer = bufnr, remap = false }
 
-			-- vim.keymap.set("n", "gd", function()
-			-- 	vim.lsp.buf.definition()
-			-- end, opts)
+			vim.keymap.set("n", "gd", function()
+				vim.lsp.buf.definition()
+			end, opts)
 			vim.keymap.set("n", "gD", function()
 				vim.lsp.buf.declaration()
 			end, opts)
-			-- vim.keymap.set("n", "K", function()
+			-- vim.keymap.set("n", "K", function()lsp
 			-- 	vim.lsp.buf.hover()
 			-- end, opts)
 			vim.keymap.set("n", "<leader>vws", function()
