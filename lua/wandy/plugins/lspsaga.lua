b4f1beb22a3dff8b12f1655208ecfe9be86d24b8
wandy <wandy.irawan17@gmail.com>
2024-01-18 13:52:19 +0700

    fix lsp

---
+++ lua/wandy/plugins/lspsaga.lua
@@ -0,0 +1,10 @@
+return {
+    "nvimdev/lspsaga.nvim",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter", -- optional
+        "nvim-tree/nvim-web-devicons", -- optional
+    },
+    config = function()
+        require("lspsaga").setup({})
+    end,
+}
