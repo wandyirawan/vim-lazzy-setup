26f7ca2f58e7cf5dda11666ab6445f8050217297
Wandy <wandy.irawan17@gmail.com>
2024-01-27 17:01:35 +0700

    remap scroll e

--- lua/wandy/plugins/harpoon.lua
+++ lua/wandy/plugins/harpoon.lua
@@ -10,7 +10,7 @@ return {
 		local ui = require("harpoon.ui")
 
 		vim.keymap.set("n", "<leader>a", mark.add_file)
-		vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+		vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu)
 
 		vim.keymap.set("n", "<C-n>", function()
 			ui.nav_file(1)
