d77f8dbc15180ca07ad76f83e655874c4f785219
Cecep Wandy <wandy.irawan17@gmail.com>
2024-06-24 10:25:17 +0700

    use cat color theme

--- lua/wandy/plugins/colorscheme.lua
+++ lua/wandy/plugins/colorscheme.lua
@@ -1,8 +1,9 @@
 return {
-    "NLKNguyen/papercolor-theme",
+    -- "NLKNguyen/papercolor-theme",
+    "catppuccin/nvim",
     priority = 1000,
     config = function()
-        vim.cmd([[colorscheme PaperColor]])
+        vim.cmd([[colorscheme catppuccin]])
         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end,
